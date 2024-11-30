f944bb6c4942e1bfb7b04e4642f5f5343a3abd98
roomie00 <ocar1115.shin@gmail.com>
2024-12-01 02:00:40 +0900

    edit webconfig

--- src/main/java/com/roomie/db_connect_spring/config/WebConfig.java
+++ src/main/java/com/roomie/db_connect_spring/config/WebConfig.java
@@ -1,16 +1,23 @@
 package com.roomie.db_connect_spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://hdreactbucket.s3-website.ap-northeast-2.amazonaws.com")
-                .allowedMethods("GET", "POST")  // 허용할 HTTP method
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://hdreactbucket.s3-website.ap-northeast-2.amazonaws.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP method
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
